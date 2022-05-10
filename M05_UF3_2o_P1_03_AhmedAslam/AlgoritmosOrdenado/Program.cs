1a8e1e758e47aa66772cb377900e9e62bf4bd5a9
ahmedfanatic287 <83654271+ahmedfanatic287@users.noreply.github.com>
2022-05-10 18:57:26 +0200

    Add files via upload

---
+++ M05_UF3_2o_P1_03_AhmedAslam/AlgoritmosOrdenado/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AlgoritmosOrdenado
+{
+    public class ArraySort
+    {
+        public int[] array;
+        public int[] arrayIncreasing;
+        public int[] arrayDecreasing;
+
+        public ArraySort(int elements, Random random)
+        {
+            array = new int[elements];
+            arrayIncreasing = new int[elements];
+            arrayDecreasing = new int[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                array[i] = random.Next();
+            }
+            Array.Copy(array, arrayIncreasing, elements);
+            Array.Sort(arrayIncreasing);
+
+            Array.Copy(arrayIncreasing, arrayDecreasing, elements);
+            Array.Reverse(arrayDecreasing);
+        }
+
+        public void Benchmark(Action<int[]> function)
+        {
+            int[] temp = new int[array.Length];
+            Array.Copy(array, temp, array.Length);
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine(function.Method.Name);
+
+            stopwatch.Start();
+            function(temp);
+            stopwatch.Stop();
+            Console.WriteLine("Random: " + stopwatch.ElapsedMilliseconds + "ms " + stopwatch.ElapsedTicks + "ticks");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+            function(temp);
+            stopwatch.Stop();
+            Console.WriteLine("Increasing: " + stopwatch.ElapsedMilliseconds + "ms " + stopwatch.ElapsedTicks + "ticks");
+            stopwatch.Reset();
+
+            Array.Reverse(temp);
+            stopwatch.Start();
+            function(temp);
+            stopwatch.Stop();
+            Console.WriteLine("Decreasing: " + stopwatch.ElapsedMilliseconds + "ms " + stopwatch.ElapsedTicks + "ticks");
+        }
+
+        public void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void BubbleSortEarlyExit(int[] arr)
+        {
+            bool isOrdered = true;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                isOrdered = true;
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if(arr[j] > arr[j + 1])
+                    {
+                        isOrdered = false;
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+                if (isOrdered)
+                    return;
+            }
+        }
+        public void QuickSort(int[] arr)
+        {
+            QuickSort(arr, 0, arr.Length - 1);
+        }
+        public void QuickSort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivot = QuickSortIndex(arr, left, right);
+                QuickSort(arr, left, pivot);
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+        public int QuickSortIndex(int[] arr, int left, int right)
+        {
+            int pivot = arr[(left + right) / 2];
+
+            while (true)
+            {
+                while(arr[left] < pivot)
+                {
+                    left++;
+                }
+                while(arr[right] > pivot)
+                {
+                    right--;
+                }
+                if(right <= left)
+                {
+                    return right;
+                }
+                else
+                {
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    right--;left++;
+                }
+            }
+        }
+        public void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int val = arr[i];
+                int flag = 0;
+                for (int j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+        }
+        public void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                var smallestVal = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[smallestVal])
+                    {
+                        smallestVal = j;
+                    }
+                }
+                var tempVar = arr[smallestVal];
+                arr[smallestVal] = arr[i];
+                arr[i] = tempVar;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers do you want?");
+            int elements = int.Parse(Console.ReadLine());
+            Console.WriteLine("What seed do you want to use?");
+            int seed = int.Parse(Console.ReadLine());
+            Random random = new Random(seed);
+            ArraySort arraySort = new ArraySort(elements, random);
+
+            //arraySort.Benchmark(arraySort.BubbleSort);
+            arraySort.Benchmark(arraySort.BubbleSortEarlyExit);
+            arraySort.Benchmark(arraySort.QuickSort);
+            arraySort.Benchmark(arraySort.InsertionSort);
+            arraySort.Benchmark(arraySort.SelectionSort);
+
+        }
+    }
+}
---
+++ M05_UF3_2o_P1_03_AhmedAslam/TestOrdenado/UnitTest1.cs
@@ -0,0 +1,74 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AlgoritmosOrdenado;
+using System.Linq;
+
+namespace TestOrdenado
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BubbleSort_Test()
+        {
+            //Arrange
+            Random random = new Random();
+            ArraySort arraySort = new ArraySort(10000, random);
+            int[] temp = new int[arraySort.array.Length];
+            Array.Copy(arraySort.array, temp, arraySort.array.Length);
+
+            //Act
+            arraySort.BubbleSort(temp);
+
+            //Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(temp, arraySort.arrayIncreasing), "No ha ordenado bien");
+        }
+        [TestMethod]
+        public void QuickSort_Test()
+        {
+            //Arrange
+            Random random = new Random();
+            ArraySort arraySort = new ArraySort(10000, random);
+            int[] temp = new int[arraySort.array.Length];
+            Array.Copy(arraySort.array, temp, arraySort.array.Length);
+
+            //Act
+            arraySort.QuickSort(temp);
+
+            //Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(temp, arraySort.arrayIncreasing), "No ha ordenado bien");
+        }
+        public void InsertionSort_Test()
+        {
+            //Arrange
+            Random random = new Random();
+            ArraySort arraySort = new ArraySort(10000, random);
+            int[] temp = new int[arraySort.array.Length];
+            Array.Copy(arraySort.array, temp, arraySort.array.Length);
+
+            //Act
+            arraySort.InsertionSort(temp);
+
+            //Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(temp, arraySort.arrayIncreasing), "No ha ordenado bien");
+        }
+
+        public void SelectionSort_Test()
+        {
+            //Arrange
+            Random random = new Random();
+            ArraySort arraySort = new ArraySort(10000, random);
+            int[] temp = new int[arraySort.array.Length];
+            Array.Copy(arraySort.array, temp, arraySort.array.Length);
+
+            //Act
+            arraySort.SelectionSort(temp);
+
+            //Assert
+            Assert.IsTrue(Enumerable.SequenceEqual(temp, arraySort.arrayIncreasing), "No ha ordenado bien");
+        }
+
+
+    }
+        
+}
